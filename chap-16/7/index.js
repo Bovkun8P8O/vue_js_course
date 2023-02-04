9cf33dcdb983276e278ca3d6cd77f1c589ae71fb
paul <cherepaha325@gmail.com>
2023-02-04 15:59:16 +0200

    chapter 16 lesson 7

---
+++ chap-16/7/index.js
@@ -0,0 +1,20 @@
+let fs = require('fs');
+
+fs.writeFileSync('test.txt', 'Hello world!');
+
+let data = fs.readFileSync('test.txt', {ENCODING: 'UTF-8'});
+console.log(data.toString());
+
+fs.writeFile('text.txt', 'Hello world again!', function (err) {
+    if (err) throw new Error(err);
+
+    fs.rename('text.txt', 'text2.txt', function (err) {
+        if (err) throw new Error(err);
+
+        fs.readFile('text2.txt', {encoding: 'utf-8'}, function (err, data) {
+            if (err) throw new Error(err);
+
+            console.log(data);
+        })
+    })
+})
