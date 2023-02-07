91db222559b8fd09ca916a1bf418e4ccfe385202
paul <cherepaha325@gmail.com>
2023-02-07 11:58:08 +0200

    chapter 18 lesson 2

---
+++ chap-18/02/index.js
@@ -0,0 +1,37 @@
+// var q = 1;
+//
+// q = 2;
+//
+// q = 'w';
+
+// const MY_CONST = 1;
+// MY_CONST = 2;
+
+// const MY_CONST = [1, 2, 3];
+//
+// MY_CONST[2] = 4;
+// console.log(MY_CONST);
+
+// MY_CONST = '4';
+// console.log(MY_CONST);
+
+// const MY_CONST = {
+//     name: 'WFM';
+// };
+//
+// MY_CONST = 1;
+//
+// MY_CONST.name = 1;
+
+// let a = 1;
+//
+// a = '2';
+
+for (/*var*/ let i = 0; i < 5; i++) {
+    setTimeout(function () {
+        console.log(i);
+    }, 3000);
+}
+
+const PI = 3.14;
+let a = 1;
