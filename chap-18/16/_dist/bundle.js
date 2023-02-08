42fb6cae3c66fe6d644c81f2f0d5a584aaf2ffcb
paul <cherepaha325@gmail.com>
2023-02-08 16:12:38 +0200

    chapter 18 lesson 16

---
+++ chap-18/16/_dist/bundle.js
@@ -0,0 +1,105 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+// let obj1 = {a: 1};
+// let obj2 = {b: 2, c: 3};
+
+// Object.assign(obj1, obj2);
+
+// let obj3 = Object.assign({d: 4}, obj1, obj2);
+
+// console.log('Obj1', obj1);
+// console.log('Obj2', obj2);
+// console.log('Obj3', obj3);
+
+// let foundItem = [1, 2, 3, 4].find(x => x > 2);
+// console.log(foundItem); // 3
+
+var str = 'Hello!';
+
+console.log('Repeat: ', str.repeat(3)); // Hello!Hello!Hello!
+console.log('StartsWith: ', str.startsWith('el', 1)); // true
+console.log('Includes: ', str.includes('llo', 2)); // true
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(0);
+(function webpackMissingModule() { throw new Error("Cannot find module \"build\""); }());
+
+
+/***/ })
+/******/ ]);
---
+++ chap-18/16/index.js
@@ -0,0 +1,19 @@
+// let obj1 = {a: 1};
+// let obj2 = {b: 2, c: 3};
+
+// Object.assign(obj1, obj2);
+
+// let obj3 = Object.assign({d: 4}, obj1, obj2);
+
+// console.log('Obj1', obj1);
+// console.log('Obj2', obj2);
+// console.log('Obj3', obj3);
+
+// let foundItem = [1, 2, 3, 4].find(x => x > 2);
+// console.log(foundItem); // 3
+
+let str = 'Hello!';
+
+console.log('Repeat: ', str.repeat(3)); // Hello!Hello!Hello!
+console.log('StartsWith: ', str.startsWith('el', 1)); // true
+console.log('Includes: ', str.includes('llo', 2)); // true
