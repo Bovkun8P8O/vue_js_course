61104ffa14fe331fa11a0fbbbbad239ace0be9bf
paul <cherepaha325@gmail.com>
2023-02-08 13:35:19 +0200

    chapter 18 lesson 13

---
+++ chap-18/13/extra.js
@@ -0,0 +1,16 @@
+// /*export*/ let name = 'WFM';
+// /*export*/ let age = 20;
+//
+// export {
+//     age, name,
+// }
+
+export default function func() {
+    console.log('I am func.');
+}
+
+export default class Car {
+    constructor() {
+        console.log('Car constructor!');
+    }
+}
---
+++ chap-18/13/index.js
@@ -0,0 +1,11 @@
+// import /** as extra*/ {name, age/* as a*/} from './extra';
+
+// console.log(/*extra*/age, name);
+
+import /*{*/func/*}*/ from "./extra";
+
+func();
+
+import Car from './extra';
+
+let car = new Car();
